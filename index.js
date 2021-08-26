ac8611cee7a4bcefe14a3d06605c8202a1cab17c
QmelZ <59574967+QmelZ@users.noreply.github.com>
2021-08-26 21:15:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-async function getIPs(){
-    const PeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-    const conn = new PeerConnection({iceServers: []});
-    const ips = new Set();
-    const regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g;
-    
-    // conn.createDataChannel("");
-    
-    const {sdp} = await conn.createOffer();
-    
-    sdp.split('\n').forEach(e => {
-        if(e.indexOf("candidate") === -1) return;
-        e.match(ipRegex).forEach(e => ips.add(e));
-    });
-    
-    return ips;
+async function getIP(){
+    const ip = await fetch("https://icanhazip.com/").then(e => e.clone().text());
+    return ip.slice(0, -1);
 }
 
 const cont = document.querySelector(".cont");
-const ips = await getIPs();
-cont.innerText = `your ip addresses are:
-${(...ips).join("\n")}`;
+const ip = await getIP();
+cont.innerText = `your ip address is: ${ip}`;
