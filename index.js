e23ec9f50f0f898573275845b81e4cd21e033db0
QmelZ <59574967+QmelZ@users.noreply.github.com>
2021-08-26 20:36:20 +0300

    how to not type

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ async function getIPs(){
     const PeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
     const conn = new PeerConnection({iceServers: []});
     const ips = new Set();
-    const regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+    const regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g;
     
     conn.createDataChannel("");
     
